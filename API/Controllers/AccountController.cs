2aa94b926f7032e3bc65512ce942f865b4bb0f6d
epiq122 <robgleason122@gmail.com>
2024-08-26 18:38:52 -0700

    created the user dto to manage our jwt, updated the account controller to use the dto

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using API.Data;
 using API.DTO;
+using API.Interfaces;
 using API.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -8,10 +9,10 @@ using System.Text;
 
 namespace API.Controllers
 {
-	public class AccountController(DataContext context) : BaseApiController
+	public class AccountController(DataContext context, ITokenService tokenService) : BaseApiController
 	{
 		[HttpPost("register")] // account/register
-		public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+		public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
 		{
 
 			if (await UserExists(registerDto.Username)) return BadRequest("Username is taken!");
@@ -26,11 +27,15 @@ namespace API.Controllers
 			context.Users.Add(user);
 			await context.SaveChangesAsync();
 
-			return user;
+			return new UserDto
+			{
+				Username = user.Username,
+				Token = tokenService.CreateToken(user),
+			};
 		}
 
 		[HttpPost("login")]
-		public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+		public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
 		{
 			var user = await context.Users.FirstOrDefaultAsync(x => x.Username == loginDto.Username.ToLower());
 
@@ -44,7 +49,11 @@ namespace API.Controllers
 				if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
 			}
 
-			return user;
+			return new UserDto
+			{
+				Username = user.Username,
+				Token = tokenService.CreateToken(user),
+			};
 
 
 
