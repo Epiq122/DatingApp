0ec74ebbdb5e70db534a667e53ed205441cf31b7
epiq122 <robgleason122@gmail.com>
2024-09-04 18:38:05 -0700

    added a toastr to better show when you login and register using toastr

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,6 +3,7 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -14,14 +15,16 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 export class NavComponent {
   accountService = inject(AccountService);
   private router = inject(Router);
+  private toastr = inject(ToastrService);
   model: any = {};
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: (_) => {
         this.router.navigateByUrl('/members');
+        this.toastr.success('successfully logged in!');
       },
-      error: (error) => console.log(error),
+      error: (error) => this.toastr.error(error.error),
     });
   }
 
