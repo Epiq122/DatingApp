c29c11b2286f2ab579aa7e3f519105a267dca5bf
epiq122 <robgleason122@gmail.com>
2024-09-04 19:35:55 -0700

    created a buggy controll on the api side to showcase different errors

--- API/DTO/RegisterDto.cs
+++ API/DTO/RegisterDto.cs
@@ -6,10 +6,11 @@ namespace API.DTO
 	{
 
 		[Required]
-		public required string Username { get; set; }
+		public string Username { get; set; } = string.Empty;
 
 		[Required]
-		public required string Password { get; set; }
+		[StringLength(8, MinimumLength = 8)]
+		public string Password { get; set; } = string.Empty;
 
 	}
 }
