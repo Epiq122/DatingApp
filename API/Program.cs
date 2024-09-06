2ea19b0cb337bcaaa047137239bc6a22de47a868
Robert Gleason <l2ob122@gmail.com>
2024-09-05 20:04:24 -0700

    created a seed class, and setup code for migrating and seeding

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,5 +21,22 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// seed users - migrations
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+  var context = services.GetRequiredService<DataContext>();
+  await context.Database.MigrateAsync();
+  await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+
+  var logger = services.GetRequiredService<ILogger<Program>>();
+  logger.LogError(ex, "an error occured during migration");
+}
 
 app.Run();
