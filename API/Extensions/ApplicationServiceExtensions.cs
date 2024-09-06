bd47ea563445c0722c26a1d05a7c61e034a571e5
Robert Gleason <l2ob122@gmail.com>
2024-09-05 20:42:15 -0700

    created the Iuserrepo and user repo

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,4 +1,5 @@
-﻿using API.Data;
+﻿using API.Controllers;
+using API.Data;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
@@ -18,6 +19,7 @@ namespace API.Extensions
 
 			services.AddCors();
 			services.AddScoped<ITokenService, TokenService>();
+			services.AddScoped<IUserRepository, UserRepository>();
 
 			return services;
 		}
